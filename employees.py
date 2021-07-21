f4bea786b269af4b268ea17a7b7cbe6a11170163
Ramesh Ram <ramesh.r3@ibm.com>
2021-07-21 09:49:03 +0530

    employees adding

---
+++ employees.py
@@ -0,0 +1,9 @@
+employees=["dhoni","raina","virat",7,10,3.18]
+del employees[3]
+print(employees)
+employees.remove("virat")
+print(employees)
+employees.pop(1)
+print(employees)
+employees.clear()
+print(employees)    
---
+++ sum.py
@@ -0,0 +1,3 @@
+a,b,c=10,20,30
+sum1=a+b+c
+print(sum1)
